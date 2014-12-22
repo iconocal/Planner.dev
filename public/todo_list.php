b34ac8b4c00e462d4025bcca3ec1f283a379007f
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2014-12-22 14:24:49 -0600

    todo list.php with upload files functionality

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,18 +1,39 @@
 <?php
 
-	$todo_array = [];
+		$todo_array = openfile('data/todo.txt');
+
+	// Verify there were uploaded files and no errors
+	if (count($_FILES) > 0 && $_FILES['file1']['error'] == UPLOAD_ERR_OK) {
+	    // Set the destination directory for uploads
+	    $uploadDir = '/vagrant/sites/planner.dev/public/uploads/';
+
+	    // Grab the filename from the uploaded file by using basename
+	    $filename = basename($_FILES['file1']['name']);
+
+	    // Create the saved filename using the file's original name and our upload directory
+	    $savedFilename = $uploadDir . $filename;
+
+	    // Move the file from the temp location to our uploads directory
+	    move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
+
+	    $upload_array = openfile($savedFilename);
+
+
+	    $todo_array = array_merge($todo_array, $upload_array);
+	    writeFile('data/todo.txt', $todo_array);
+
+	}
+
+
 
 	function writeFile($filename, $arrayList) {
-	// $filename = $path;
-	$handle = fopen($filename, 'w');
-	foreach ($arrayList as $listItem) {
-		fwrite($handle, $listItem . PHP_EOL);
-		}
-	fclose($handle);
-}
+		$handle = fopen($filename, 'w');
+		foreach ($arrayList as $listItem) {
+			fwrite($handle, $listItem . PHP_EOL);
+			}
+		fclose($handle);
+	}
 
-
-	// //FUTURE OPTION
 	function openfile($filename) {
 		$handle = fopen($filename, 'r');
 		$contents = trim(fread($handle, filesize($filename)));
@@ -21,20 +42,23 @@
 		return $todo_array;
 	}
 
-	$todo_array = openfile('data/todo.txt');
+
+
+
 
 	if(isset($_POST['todo'])) {
 		$todo_array[] = $_POST['todo'];
-		writeFile('data/todo.txt', $todo_array);
-		
+		writeFile('data/todo.txt', $todo_array);	
 	}
-
 
 	if(isset($_GET['remove'])) {
-	$id = $_GET['remove'];
-	unset($todo_array[$id]);
-	writeFile('data/todo.txt', $todo_array);
+		$id = $_GET['remove'];
+		unset($todo_array[$id]);
+		writeFile('data/todo.txt', $todo_array);
 	}
+
+	
+
 ?>
 
 
@@ -53,6 +77,10 @@
 <div class="container">
 
 	<h1>Least Fun TO DO List Ever</h1>
+
+	
+
+    
 
 	<ul>
 		<?php
@@ -75,6 +103,20 @@
 
 	    <button type="submit">Add Task</button>
 	</form>
+
+	<h2>Upload TO DO List</h2>
+
+
+
+	<form method="POST" enctype="multipart/form-data">
+        <p>
+            <label for="file1">File to upload: </label>
+            <input type="file" id="file1" name="file1">
+        </p>
+        <p>
+            <input type="submit" value="Upload">
+        </p>
+    </form>
 
 </div>
 	
